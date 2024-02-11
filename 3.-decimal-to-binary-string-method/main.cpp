f7b6d6f7d08afbf09adbbcef0158db05f3666227
Adinath Mahangare <adinathmahangare914@gmail.com>
2024-02-11 19:28:37 +0530

    Add files via upload

---
+++ 3.-decimal-to-binary-string-method/main.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+It’s another efficient approach to converting Decimal to binary 
+using the right shift(>>) and And(&) operator. 
+Here we’ll use only Binary Operators which 
+usually are very fast in computation.
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+string decimalToBinary(int num)
+{
+    string str;
+    
+    while(num){
+        if (num & 1){
+            str+='1';
+        }else{
+            str+='0';
+        }
+        
+        num>>=1;    //right shift by 1
+    }
+    return str;
+}
+
+void reverseString(string str){
+    for (int i=str.size()-1; i>-1; --i){
+        cout<<str[i];
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter a decimal number:";
+    cin>>n;
+    
+    cout<<"Binary conversion of number is:";
+    reverseString(decimalToBinary(n));
+
+    return 0;
+}
